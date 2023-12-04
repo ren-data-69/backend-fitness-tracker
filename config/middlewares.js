a48d837f1660c588ac9417e8a11d9f676c4aa2e4
ren-data-69 <152914028+ren-data-69@users.noreply.github.com>
2023-12-05 02:30:40 +0530

    Update middlewares.js final

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,23 +1,12 @@
 module.exports = [
-  "strapi::errors",
-  "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: [
-        "http://localhost:3000",
-        "https://fitness-tracker-sujal.vercel.app",
-      ], // Add your allowed origins
-      credentials: true, // Allow credentials (optional)
-      methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"], // Specify allowed methods
-      headers: ["Content-Type", "Authorization"], // Specify allowed headers
-    },
-  },
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
